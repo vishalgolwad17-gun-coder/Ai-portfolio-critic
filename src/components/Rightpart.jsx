8601e1657d912a48041fc817634f1012e3ae48d7
Vishal Amol Golwad <vishalgolwad17@gmail.com>
2026-07-03 20:24:08 +0530

    Move images to public folder and fix image paths

--- src/components/Rightpart.jsx
+++ src/components/Rightpart.jsx
@@ -3,7 +3,7 @@ const Rightpart = () => {
   return (
     <div className='md:flex hidden w-full h-screen mt-8'>
       <div className=' md:h-[82vh] h-screen w-full flex justify-center items-center '>
-        <img src="/Projectimages/project image.png" alt="" className='shadow-2xl rounded-xl shadow-purple-700' />
+        <img src="/Projectimages/projectimage.png" alt="" className='shadow-2xl rounded-xl shadow-purple-700' />
       </div>
     </div>
   )
